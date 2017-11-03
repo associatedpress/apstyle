ec5f8218e330a419f96ffeeebc2475a6b81dd759
lfenn <lfenn@ap.org>
2017-11-03 17:55:21 -0400

    Add replacement render function

---
+++ R/rmd_render.R
@@ -0,0 +1,16 @@
+#' Rmarkdown render replacement
+#'
+#' The default Rmarkdown render in Rstudio doesn't play nice with our datakit
+#' project structure. This replacement function does a better job of separating
+#' the source .Rmd file from the output .html file
+#' @import rmarkdown
+#' @import aptheme
+
+#' @export
+render <- function(input, encoding, project_dir) {
+  rmarkdown::render(
+    input,
+    encoding = encoding,
+    knit_root_dir = project_dir,
+    output_dir = file.path(project_dir, "data/rmd_output"))
+}
